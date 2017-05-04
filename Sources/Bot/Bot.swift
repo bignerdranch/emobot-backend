e77cab1db4b3f6c6380ff2f242ab38ffe50695ac
Josh Justice <jjustice@bignerdranch.com>
2017-05-04 15:20:33 -0400

    WIP detect a kudo

--- Sources/Bot/Bot.swift
+++ Sources/Bot/Bot.swift
@@ -1,3 +1,4 @@
+import Foundation
 import HTTP
 import Vapor
 
@@ -24,15 +25,26 @@ class Bot {
                 
                 let event = try JSON(bytes: text.utf8.array)
                 guard
+                    let fromUser = event["user"]?.string,
                     let channel = event["channel"]?.string,
                     let text = event["text"]?.string
                     else { return }
-                
+
                 if text.hasPrefix("hello") {
                     let response = SlackMessage(to: channel, text: "Hi there 👋")
                     try ws.send(response)
+                    return
                 } else if text.hasPrefix("version") {
                     let response = SlackMessage(to: channel, text: "Current Version: \(VERSION)")
+                    try ws.send(response)
+                    return
+                }
+                
+                let kudoRegex = try NSRegularExpression(pattern: "(\\w+)\\+\\+\\s+(.*)")
+                if let match = kudoRegex.actuallyUsableMatch(in: text) {
+                    let toUser = match.captures[0]
+                    let description = match.captures[1]
+                    let response = SlackMessage(to: channel, text: "\(fromUser) sent kudos to \(toUser) in \(channel) for \(description)")
                     try ws.send(response)
                 }
             }
@@ -41,6 +53,23 @@ class Bot {
                 print("\n[CLOSED]\n")
             }
         }
+    }
+}
+
+extension NSRegularExpression {
+    func actuallyUsableMatch(in string: String) -> (fullMatch: String, captures: [String])? {
+        let nsString = string as NSString
+        let range = NSMakeRange(0, nsString.length)
+        guard let match = firstMatch(in: string, range: range) else {
+            return nil
+        }
+        
+        let fullMatch = nsString.substring(with: match.range)
+        var captures: [String] = []
+        for i in 1 ..< match.numberOfRanges {
+            captures.append(nsString.substring(with: match.rangeAt(i)))
+        }
+        return (fullMatch, captures)
     }
 }
 
